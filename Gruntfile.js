088539d788b8bf41e50c192eed79af69a4262f02
Edward Comley <ebcomley@gmail.com>
2013-12-20 15:25:35 +0000

    Adding the initial build files required for starting a bootstrap less development or a sass compass susy development.

---
+++ Gruntfile.js
@@ -0,0 +1,157 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-push-release');
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    less: {
+      options: {
+          paths: ["bower_components/bootstrap/less", "less"],
+      },
+      development: {
+        files: {
+          "css/less-example.css": "less/less-example.less"
+        }
+      }, // development
+      production: {
+        options: {
+          paths: ["bower_components/bootstrap/less", "less"],
+          cleancss: true
+        },
+        files: {
+          "css/dist/less-example.min.css": "less/less-example.less"
+        }
+      } // production
+    }, // less
+
+    compass: {
+
+      development: {
+        options: {
+          config: 'config.rb',
+          cssDir: 'css',
+        } // options
+      }, // development
+
+      production: {
+        options: {
+          config: 'config_prod.rb',
+          cssDir: 'css/dist',
+        }, // options
+      } // production
+
+    }, //compass
+
+    jshint: {
+      beforeconcat: ['js/*.js']
+    }, // jshint
+
+    concat: {
+      dist: {
+        src: [
+          'js/libs/*.js',
+          'bower_components/bootstrap/js/*.js',
+          'js/scripts.js'
+        ],
+        dest: 'js/scripts.expanded.js',
+      }
+    },
+
+    uglify: {
+      build: {
+        src: 'js/scripts.expanded.js',
+        dest: 'js/scripts.min.js' // for some reason this places scripts.js before jquery
+      }
+    },
+
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'images/'
+        }]
+      }
+    },
+
+    watch: {
+      
+      options: {
+        livereload: true,
+      }, //options
+
+      // Add the following script to your HTML for livereload.
+      // <script src="http://localhost:35729/livereload.js"></script>
+
+      scripts: {
+        files: ['js/*.js'],
+        tasks: ['concat', 'uglify', 'jshint'],
+        options: {
+          spawn: false,
+        }
+      }, // scripts
+
+      css: {
+        files: ['less/*.less'],
+        tasks: ['less'],
+      }, // css
+      
+      sass: {
+        files: ['sass/*.scss'],
+        tasks: ['compass:development','compass:production' ]
+      }, // sass
+
+      images: {
+        files: ['images/**/*.{png,jpg,gif}', 'images/*.{png,jpg,gif}'],
+        tasks: ['imagemin'],
+        options: {
+          spawn: false,
+        }
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: './'
+        }
+      }
+    },
+
+
+   push: {
+    options: {
+      files: ['package.json'],
+      updateConfigs: [],
+      releaseBranch: false,
+      add: true,
+      addFiles: ['.'], // '.' for all files except ingored files in .gitignore
+      commit: true,
+      commitMessage: 'Release v%VERSION%',
+      commitFiles: ['-a'], // '-a' for all files
+      createTag: true,
+      tagName: 'v%VERSION%',
+      tagMessage: 'Version %VERSION%',
+      push: true,
+      pushTo: 'origin',
+      npm: false,
+      npmTag: 'Release v%VERSION%',
+      gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+    } //options
+  }
+
+
+
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  // Default Task is basically a rebuild
+  grunt.registerTask('default', ['concat', 'uglify', 'less', 'compass', 'imagemin', 'watch']);
+
+  grunt.registerTask('dev', ['connect', 'watch']);
+
+};
--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 design-boilerplate
 ==================
 
-A designers boilerplate using; grunt, sass or less.
+A boilerplate which options for using either:
+
+- Less with bootstrap
+- Sass with compass and susy
+
+
+#### Setup:
+
+    $ npm install
+    $ bower install
+    $ grunt dev
+
+#### Available with this boilerplate:
+
+Recommend you don't use all at once. Bootstrap or Compass with Susy.
+
+- [Bootstrap](http://getbootstrap.com/)
+- [Compass Home | Compass Documentation](http://compass-style.org/)
+- [Susy: Responsive grids for Compass](http://susy.oddbird.net/)
+
+
+##### Futher information on:
+
+- [JonnyBGod/grunt-push-release](https://github.com/JonnyBGod/grunt-push-release)
+
